a7d6e7fdcaf502fa56f6246fbb721d056a2a60d4
Moka Rupa Devi <134187232+rupamoka@users.noreply.github.com>
2025-10-30 12:02:50 +0530

    Remove insert and select statements from SQL script
    Removed insert values and select statements from the SQL script.

--- 1. _create_database.sql
+++ 1. _create_database.sql
@@ -21,31 +21,3 @@ desc student;
 | Grade | varchar(4)  | YES  |     | NULL    |       |
 +-------+-------------+------+-----+---------+-------+
 
---Insert values--
-insert into student values
-(1,'Ram',20,'B'),(2,'Sreeteja',20,'B'),(3,'Ravi',19,'C'),
-(4,'Pavan',19,'A'),(5,'Meena',20,'D'),(6,'Durga',19,'A'),
-(7,'Kiran',20,'A'),(8,'Manasa',21,'A'), (9,'Teja',19,'C'),
-(10,'Vasanth',20,'B'),(11,'Nissy',21,'B'),(12,'Lokesh',21,'C')
- ;
-
---To view the table--
-select * from student;
-
---Output--
-+------+----------+------+-------+
-| Id   | Name     | Age  | Grade |
-+------+----------+------+-------+
-|    1 | Ram      |   20 | B     |
-|    2 | Sreeteja |   20 | B     |
-|    3 | Ravi     |   19 | C     |
-|    4 | Pavan    |   19 | A     |
-|    5 | Meena    |   20 | D     |
-|    6 | Durga    |   19 | A     |
-|    7 | Kiran    |   20 | A     |
-|    8 | Manasa   |   21 | A     |
-|    9 | Teja     |   19 | C     |
-|   10 | Vasanth  |   20 | B     |
-|   11 | Nissy    |   21 | B     |
-|   12 | Lokesh   |   21 | C     |
-+------+----------+------+-------+
